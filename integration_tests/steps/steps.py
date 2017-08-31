e4aa23f3dff3fd04be2f92ae81be6d4e15dd240b
Pavel Kajaba <pkajaba@redhat.com>
2017-08-31 12:23:02 +0200

    Add comment about duration of tests

--- integration_tests/steps/steps.py
+++ integration_tests/steps/steps.py
@@ -10,6 +10,7 @@ def step_impl(context):
 def step_impl(context, count):
     event_count = 0
     for e in context.firehose_container.logs(stream=True):
+        # Note that this might take some time since it depends on libraries.io
         if "Received event for package" in e.decode('utf-8'):
             event_count = event_count + 1
 
