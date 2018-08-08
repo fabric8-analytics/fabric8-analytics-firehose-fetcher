74535723d997a2281a65d527d938eabda11a2ccf
Michal Srb <michal@redhat.com>
2018-08-08 19:35:28 +0200

    Default to HTTPS

--- f8a_firehose_fetcher/defaults.py
+++ f8a_firehose_fetcher/defaults.py
@@ -2,6 +2,6 @@
 
 import os
 
-STREAM_URL = os.environ.get('FIREHOSE_STREAM_URL', 'http://firehose.libraries.io/events')
+STREAM_URL = os.environ.get('FIREHOSE_STREAM_URL', 'https://firehose.libraries.io/events')
 ENABLE_SCHEDULING = os.environ.get('ENABLE_SCHEDULING', '1') in ('1', 'True', 'true')
 PROBE_FILE_LOCATION = "/tmp/firehose-fetcher/liveness.txt"
